9986edaccfceccd8d96403eb73af10169c64ed10
jerry <jerry110160@gmail.com>
2018-07-29 16:31:26 -0400

    test

---
+++ misc/keyword/source/customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyword.source
+{
+    class customer
+    {
+
+        string name { get; set; }
+        int age { get; set; }
+
+        public customer(string _name, int _age)
+        {
+            name = _name;
+            age = _age;
+        }
+    }
+}
---
+++ misc/keyword/source/customerStrut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyword.source
+{
+
+
+    public struct customerStrut
+    {
+        public int x, y;
+
+        public customerStrut(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+    }
+}
